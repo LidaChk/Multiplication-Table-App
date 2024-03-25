29e0b414b3d8cf436697382ba7ce492a8cd8058c
LidaChk <lvglazko@gmail.com>
2024-03-25 09:49:07 +0300

    feat:🦄 build

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: './build',
+  output: 'export',
+  trailingSlash: true,
+};
+
+export default nextConfig;
